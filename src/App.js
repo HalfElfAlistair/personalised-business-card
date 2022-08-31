26cec68c23710703e360b2c877d847341d161dbe
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-08-31 12:03:07 +0100

    adding routes for Create and View components in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import {db} from './firebase'
+import { Routes, Route } from 'react-router-dom';
+import Create from './components/Create'
+import View from './components/View'
 
 function App() {
 
   return (
-    <div className="App">
-    </div>
+    <Routes>
+      <Route path={"/"} element={<Create />} />
+      <Route path={"/view"} element={<View />} />
+    </Routes>
   );
 }
 
