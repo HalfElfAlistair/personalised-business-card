f990ef59e7ce3cd842661ad99270b0c9dd388284
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-08-31 12:07:24 +0100

    returning card-front and card-back divs with static text where data will be presented, in View.jsx

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,3 +1,20 @@
-const View = () => {}
+const View = () => {
+
+    return <div>
+        <div id="card-front">
+            <p>Business</p>
+            <img></img>
+        </div>
+        <div id="card-back">
+            <p>Name</p>
+            <p>Job</p>
+            <p>Phone</p>
+            <p>Email</p>
+            <p>Website</p>
+        </div>
+        <button>START AGAIN</button>
+    </div>
+
+}
 
 export default View;
