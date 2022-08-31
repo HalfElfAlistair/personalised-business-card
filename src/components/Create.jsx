6addff946a4bf86c95c5c1ce3a8af7b2493f921c
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-08-31 12:03:58 +0100

    returning h1 and Form component from Create.jsx

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,3 +1,12 @@
-const Create = () => {}
+import Form from '../components/Form'
+
+const Create = () => {
+
+    return <div>
+            <h1>Create your business card</h1>
+            <Form />
+    </div>
+
+}
 
 export default Create;
