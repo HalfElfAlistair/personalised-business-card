420a6c8dbe670245695876aa03223218acf49596
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-09-01 14:28:06 +0100

    centering holding div and h1 with margin classes and setting a width and height for the div

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -2,8 +2,8 @@ import Form from '../components/Form'
 
 const Create = ({setNewCardID}) => {
 
-    return <div>
-            <h1>Create your business card</h1>
+    return <div className="mx-auto w-5/12 my-60 h-3/6">
+            <h1 className="mx-auto w-full mb-0 p-5 text-center text-3xl">Create your business card</h1>
             <Form setNewCardID={setNewCardID} />
     </div>
 
