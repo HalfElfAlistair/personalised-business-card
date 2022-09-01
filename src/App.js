a1449c824cf1e66670042a6c8e4be7c7b7b3d077
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-09-01 16:31:32 +0100

    amending route path for dynamic url when viewing cards. Also collecting id from URL with useEffect and setting newCardID to this

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import {db} from './firebase'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Create from './components/Create'
 import View from './components/View'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 function App() {
 
   const [newCardID, setNewCardID] = useState("")
 
+  let location = useLocation();
+
+  useEffect(() => {
+    // takes the path from useLocation and splits it to provide the id part of a unique URL and setNewCardID with it (if present)
+    let currentPath = location.pathname.split("view/")
+    if (currentPath.length > 1) {
+      setNewCardID(currentPath[1])
+    }
+}, [])
+
   return (
     <Routes>
-      <Route path={"/"} element={<Create setNewCardID={setNewCardID} />} />
-      <Route path={"/view"} element={<View newCardID={newCardID} />} />
+      <Route path={"/"} element={<Create setNewCardID={setNewCardID} newCardID={newCardID} />} />
+      <Route path={`/view/${newCardID}`} element={<View newCardID={newCardID} />} />
     </Routes>
   );
 }
