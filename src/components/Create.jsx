e0b184ddd03476dc662e1f0dd1e0baca77e7b95f
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-09-02 14:42:49 +0100

    deleting surplus code from h1 styling

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -3,7 +3,7 @@ import Form from '../components/Form'
 const Create = ({setNewCardID}) => {
 
     return <div className="w-full m-2 lg:w-5/12 lg:my-60 lg:h-3/6 m-auto">
-            <h1 className="text-center text-2xl sm:text-3xl lg:mb-0 lg:p-5 lg:">Create your <b>business card</b></h1>
+            <h1 className="text-center text-2xl sm:text-3xl lg:mb-0 lg:p-5">Create your <b>business card</b></h1>
             <Form setNewCardID={setNewCardID} />
     </div>
 
