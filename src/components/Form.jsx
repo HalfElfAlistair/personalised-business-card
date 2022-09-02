0a81c06f1b1d2f8110a771093e7450d59adc5447
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-09-02 14:51:17 +0100

    amending styling in Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -96,56 +96,56 @@ const Form = ({setNewCardID, newCardID}) => {
     // Form is split into nested divs to make layout easier
 
     return <form className="m-5 lg:flex lg:flex-row lg:flex-wrap lg:justify-between lg:p-5" onSubmit={(e) => submitFunc(e)}>
-        <div className="sm:w-10/12 md:w-8/12 sm:m-auto lg:w-full md:flex md:flex-row md:justify-between md:gap-10 md:my-2">
-            <div className="lg:justify-self-start md:w-6/12">
+        <div className="sm:w-10/12 sm:m-auto md:w-8/12 md:flex md:flex-row md:justify-between md:gap-10 md:my-2 lg:w-full">
+            <div className="md:w-6/12 lg:justify-self-start">
                 <label className="block text-sm leading-8" htmlFor="name-input">Name</label>
                 <input className={basicValidation(name, "bg-stone-100 rounded-3xl text-sm text-left placeholder-stone-400 py-3 pl-4 w-11/12 md:w-full")} value={name} type="text" id="name-input" name="name-input" placeholder="Type your name here" onChange={(e) => setName(e.target.value)} ></input>
                 {checkIcon(name)}
             </div>
-            <div className="lg:justify-self-end md:w-6/12">
+            <div className="md:w-6/12 lg:justify-self-end">
                 <label className="block text-sm leading-8" htmlFor="job-input">Job Title</label>
                 <input className={basicValidation(job, "bg-stone-100 rounded-3xl text-sm text-left placeholder-stone-400 py-3 pl-4 w-11/12 md:w-full")} type="text"  id="job-input" name="job-input" placeholder="Type your job title here" onChange={(e) => setJob(e.target.value)} ></input>
                 {checkIcon(job)} 
             </div>
         </div>
-        <div className="sm:w-10/12 md:w-8/12 sm:m-auto md:w-full md:my-2">
+        <div className="sm:w-10/12 sm:m-auto md:w-8/12 md:w-full md:my-2">
             <label className="block text-sm leading-8 md:m-auto md:w-8/12" htmlFor="business-input">Business name</label>
             <input className={basicValidation(business, "block bg-stone-100 rounded-3xl text-sm text-left placeholder-stone-400 py-3 pl-4 w-11/12 md:w-8/12 lg:w-full md:m-auto")} type="text"  id="business-input" name="business-input" placeholder="Type your business name here" onChange={(e) => setBusiness(e.target.value)} ></input>
             {checkIcon(business)}
         </div>
-        <div className="sm:w-10/12 md:w-8/12 sm:m-auto lg:w-full md:flex md:flex-row md:justify-between md:gap-10 md:my-2">
-            <div className="lg:justify-self-start md:w-6/12">
+        <div className="sm:w-10/12 sm:m-auto md:w-8/12 md:flex md:flex-row md:justify-between md:gap-10 md:my-2 lg:w-full">
+            <div className="md:w-6/12 lg:justify-self-start">
                 <label className="block text-sm leading-8" htmlFor="phone-input">Mobile Phone number</label>
                 <input className={basicValidation(phone, "bg-stone-100 rounded-3xl text-sm text-left placeholder-stone-400 py-3 pl-4 w-11/12 md:w-full")} type="text"  id="phone-input" name="phone-input" placeholder="Type your mobile phone number here" onChange={(e) => setPhone(phoneValidation(e.target.value))} ></input>
                 {checkIcon(phone)}
             </div>
-            <div className="lg:justify-self-end md:w-6/12">
+            <div className="md:w-6/12 lg:justify-self-end">
                 <label className="block text-sm leading-8" htmlFor="email-input">Email address</label>
                 <input className={basicValidation(email, "bg-stone-100 rounded-3xl text-sm text-left placeholder-stone-400 py-3 pl-4 w-11/12 md:w-full")} type="text"  id="email-input" name="email-input" placeholder="Type your email address here" onChange={(e) => setEmail(emailValidation(e.target.value))} ></input>
                 {checkIcon(email)}
             </div>
         </div>
-        <div className="sm:w-10/12 md:w-8/12 sm:m-auto md:w-full md:my-2">
+        <div className="sm:w-10/12 sm:m-auto md:w-8/12 md:w-full md:my-2">
             <label className="block text-sm leading-8 md:m-auto md:w-8/12" htmlFor="website-input">Your website</label>
             <input className={basicValidation(website, "block bg-stone-100 rounded-3xl text-sm text-left placeholder-stone-400 py-3 pl-4 w-11/12 md:w-8/12 lg:w-full md:m-auto")} type="text"  id="website-input" name="website-input" placeholder="Type your website address here" onChange={(e) => setWebsite(websiteValidation(e.target.value))} ></input>
             {checkIcon(website)}
         </div>
-        <div className="sm:w-10/12 md:w-8/12 sm:m-auto md:w-full md:my-2">
+        <div className="sm:w-10/12 sm:m-auto md:w-8/12 md:w-full md:my-2">
             <label className="block text-sm leading-8 md:m-auto md:w-8/12" htmlFor="logo-input">Business Logo</label>
-            <input className={basicValidation(logo, "block bg-stone-100 rounded-3xl text-sm text-left placeholder-stone-400 py-3 pl-4 w-11/12 md:w-8/12 lg:w-full md:m-auto")} type="text"  id="logo-input" name="logo-input" placeholder="Paste your logo image URL here" onChange={(e) => setLogo(websiteValidation(e.target.value))} ></input>
+            <input className={basicValidation(logo, "block bg-stone-100 rounded-3xl text-sm text-left placeholder-stone-400 py-3 pl-4 w-11/12 md:w-8/12 md:m-auto lg:w-full")} type="text"  id="logo-input" name="logo-input" placeholder="Paste your logo image URL here" onChange={(e) => setLogo(websiteValidation(e.target.value))} ></input>
             {checkIcon(logo)}
         </div>
-        <div className="sm:w-10/12 md:w-8/12 lg:w-full m-auto sm:flex sm:flex-row sm:justify-between sm:gap-1 md:gap-10 my-2">
+        <div className="m-auto my-2 sm:w-10/12 md:w-8/12 sm:flex sm:flex-row sm:justify-between sm:gap-1 md:gap-10 lg:w-full">
             <div className="w-full">
                 <label className="block text-sm leading-8" htmlFor="color-input-front-bg">Front Background Colour</label>
-                <div className="block bg-stone-100 rounded-3xl w-11/12 sm:w-10/12 md:w-full h-10">
+                <div className="block h-10 bg-stone-100 rounded-3xl w-11/12 sm:w-10/12 md:w-full">
                     <input className={basicValidation(colourFrontBg, "w-3/12 h-10")} type="color" id="color-input-front-bg" name="color-input-front-bg" onChange={(e) => setColourFrontBg(e.target.value)} />
                     {checkIcon(colourFrontBg)}
                 </div>
             </div>
             <div className="w-full">
                 <label className="block text-sm leading-8" htmlFor="color-input-front-text">Front Text Colour</label>
-                <div className="block bg-stone-100 rounded-3xl w-11/12 sm:w-10/12 md:w-full h-10">
+                <div className="block bg-stone-100 h-10 rounded-3xl w-11/12 sm:w-10/12 md:w-full">
                     <input className={basicValidation(colourFrontText, "w-3/12 h-10")} type="color" id="color-input-front-text" name="color-input-front-text" onChange={(e) => setColourFrontText(e.target.value)} />
                     {checkIcon(colourFrontText)}
                 </div>
@@ -154,21 +154,21 @@ const Form = ({setNewCardID, newCardID}) => {
         <div className="sm:w-10/12 md:w-8/12 lg:w-full m-auto sm:flex sm:flex-row sm:justify-between sm:gap-1 md:gap-10 my-2">
             <div className="w-full">
                 <label className="block text-sm leading-8" htmlFor="color-input-back-bg">Back Background Colour</label>
-                <div className="block bg-stone-100 rounded-3xl w-11/12 sm:w-10/12 md:w-full h-10">
+                <div className="block bg-stone-100 h-10 rounded-3xl w-11/12 sm:w-10/12 md:w-full">
                     <input className={basicValidation(colourBackBg, "w-3/12 h-10")} type="color" id="color-input-back-bg" name="color-input-back-bg" onChange={(e) => setColourBackBg(e.target.value)} />
                     {checkIcon(colourBackBg)}
                 </div>
             </div>
             <div className="w-full">
                 <label className="block text-sm leading-8" htmlFor="color-input-back-text">Back Text Colour</label>
-                <div className="block bg-stone-100 rounded-3xl w-11/12 sm:w-10/12 md:w-full h-10">
+                <div className="block bg-stone-100 h-10 rounded-3xl w-11/12 sm:w-10/12 md:w-full">
                     <input className={basicValidation(colourBackText, "w-3/12 h-10")} type="color" id="color-input-back-text" name="color-input-back-text" onChange={(e) => setColourBackText(e.target.value)} />
                     {checkIcon(colourBackText)}
                 </div>
             </div>
         </div>
-        <div className="sm:w-10/12 md:w-8/12 sm:m-auto md:w-full md:my-2 m-auto flex">
-            <input className="content-center m-auto bg-indigo-600 rounded-3xl text-sm text-slate-50 px-16 py-4 font-semibold  mt-10 hover:underline hover:opacity-90" type="submit" value="CREATE BUSINESS CARD" />
+        <div className="m-auto flex sm:w-10/12 md:w-full md:my-2">
+            <input className="content-center m-auto bg-indigo-600 rounded-3xl text-sm text-slate-50 px-16 py-4 font-semibold mt-10 hover:underline hover:opacity-90" type="submit" value="CREATE BUSINESS CARD" />
         </div>
     </form>
 
